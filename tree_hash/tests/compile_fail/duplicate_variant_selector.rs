e5ea1875edddf12438506c3751cbb313f9c52d08
Michael Sproul <michaelsproul@users.noreply.github.com>
2026-09-01 23:51:47 +1000

    Smol improvements to new `trybuild` tests (#52)
    * Unbreak trybuild tests on my machine
    
    * Duplicate selector test case

---
+++ tree_hash/tests/compile_fail/duplicate_variant_selector.rs
@@ -0,0 +1,11 @@
+//! More than one selector in a variant-level `tree_hash` attribute must be rejected.
+use tree_hash_derive::TreeHash;
+
+#[derive(TreeHash)]
+#[tree_hash(enum_behaviour = "compatible_union")]
+enum Foo {
+    #[tree_hash(selector = "1", selector = "2")]
+    A(u8),
+}
+
+fn main() {}
